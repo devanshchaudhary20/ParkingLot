800bd876d342c4755dec2feadde2202993bbced9
devanshchaudhary20 <devanshchaudhary1997@gmail.com>
2024-06-06 17:59:35 +0530

    Initial commit

---
+++ models/Bill.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.util.Date;
+
+public class Bill extends BaseModel{
+    private Ticket ticket;
+    private Date exitTime;
+    private Gate exitGate;
+    private Operator operator;
+    private BillStatus billStatus;
+    private FeesCalculatorStrategyType feesCalculatorStrategyType;
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public Date getExitTime() {
+        return exitTime;
+    }
+
+    public void setExitTime(Date exitTime) {
+        this.exitTime = exitTime;
+    }
+
+    public Gate getExitGate() {
+        return exitGate;
+    }
+
+    public void setExitGate(Gate exitGate) {
+        this.exitGate = exitGate;
+    }
+
+    public Operator getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Operator operator) {
+        this.operator = operator;
+    }
+
+    public BillStatus getBillStatus() {
+        return billStatus;
+    }
+
+    public void setBillStatus(BillStatus billStatus) {
+        this.billStatus = billStatus;
+    }
+
+    public FeesCalculatorStrategyType getFeesCalculatorStrategyType() {
+        return feesCalculatorStrategyType;
+    }
+
+    public void setFeesCalculatorStrategyType(FeesCalculatorStrategyType feesCalculatorStrategyType) {
+        this.feesCalculatorStrategyType = feesCalculatorStrategyType;
+    }
+}
---
+++ models/Ticket.java
@@ -0,0 +1,60 @@
+package models;
+
+import java.sql.Date;
+
+public class Ticket extends BaseModel{
+    private int ticketNumber;
+    private Date entryTime;
+    private Vehicle vehicle;
+    private ParkingSpot assignedSpot;
+    private Gate generatedAt;
+    private Operator generatedBy;
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public Date getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(Date entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public ParkingSpot getAssignedSpot() {
+        return assignedSpot;
+    }
+
+    public void setAssignedSpot(ParkingSpot assignedSpot) {
+        this.assignedSpot = assignedSpot;
+    }
+
+    public Gate getGeneratedAt() {
+        return generatedAt;
+    }
+
+    public void setGeneratedAt(Gate generatedAt) {
+        this.generatedAt = generatedAt;
+    }
+
+    public Operator getGeneratedBy() {
+        return generatedBy;
+    }
+
+    public void setGeneratedBy(Operator generatedBy) {
+        this.generatedBy = generatedBy;
+    }
+}
